0f1e72a440abf8ed12b343bd2a9ed751f57f92de
aaron yang <aaron_yang@jieyu.ai>
2023-12-21 22:49:04 +0800

    ✨ feat: 增加小红书导出脚本

--- export_xhs.sh
+++
@@ -1,4 +0,0 @@
-cd /apps/slidev_themes
-npx slidev export --format png --output /tmp/xhs /apps/zillionare/$1
-rm -f /apps/zillionare/xhs/*
-cp -r /tmp/xhs/* /apps/zillionare/xhs/
---
+++ xhs.sh
@@ -0,0 +1,11 @@
+cmd=$1
+file=$2
+
+cd /apps/slidev_themes
+if [ $cmd = "serve" ]; then
+  npx slidev /apps/zillionare/$file -t /apps/slidev_themes/theme-calendar --remote
+else
+    npx slidev export --format png -t /apps/slidev_themes/theme-calendar --output /tmp/xhs /apps/zillionare/$file
+    rm -f /apps/zillionare/xhs/*
+    cp -r /tmp/xhs/* /apps/zillionare/xhs/
+fi
