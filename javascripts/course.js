9d94d1c9b060cb98d54ea45add686ff83aef7793
aaron yang <aaron_yang@jieyu.ai>
2024-09-02 18:33:09 +0800

    Deployed 27b25d8c with MkDocs version: 1.6.0

--- javascripts/course.js
+++ javascripts/course.js
@@ -23,7 +23,7 @@ var ad = [
     '<li>超 400 个独立因子，分类精讲底层逻辑，学完带走 350+ 因子实现。</li>',
     '<li>课程核心价值观：Learning without thought is labor lost. Know-How & Know-Why.</li>',
     '<li>三大<b>实用模型</b>，奠定未来研究框架<sup>1</sup>：聚类算法寻找配对交易标的（中性策略核心）、基于 XGBoost 的资产定价、趋势交易模型。</li>',
-    '<li>领先教学手段：SBP（Slidev Based Presentation）、INI（In-place Notebook Interaction）和基于 Nbgrader（UCBerkley 使用中）的作业系统。</li>',
+    '<li>领先的教学手段：SBP（Slidev Based Presentation）、INI（In-place Notebook Interaction）和基于 Nbgrader（UCBerkley 使用中）的作业系统。</li>',
     '</ol>',
     '<hr style="border-bottom:1px solid #ccc;height:1px;width:20%">',
     '<p style="font-size:10px !important">1. 示例模型思路新颖。未来一段时间，你都可以围绕这些模型增加因子、优化参数，构建出领先的量化策略系统。</p>',
