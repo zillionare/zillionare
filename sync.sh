73b1ac7937fd7b6437d4bf89473bda46f0844769
zillionare <aaron_yang@jieyu.ai>
2025-06-23 09:24:24 +0800

    🦄 refactor: 移除zillionare-compose环境构建

--- sync.sh
+++
@@ -1,3 +0,0 @@
-# cp /apps/cheese_course/docs/courseware/notes/toc.md docs/coursea/cheese/outline.md
-# cp /apps/trader-gm-adaptor/README.md docs/products/gm-adaptor-installation.md
-cp -r /apps/best-practice-python/docs/chap*.md docs/articles/python/best-practice-python/
